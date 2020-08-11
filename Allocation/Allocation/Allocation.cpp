92a284b4ab4ec7c9c730dfe8326273d7af7bbffc
SickSixtySix <this_mail_for_social_web@mail.ru>
2020-08-11 17:46:06 +0300

    Revised data structures. Basic implementation of allocation and release functions.

--- Allocation/Allocation/Allocation.cpp
+++ Allocation/Allocation/Allocation.cpp
@@ -1,70 +1,117 @@
 ﻿#include <iostream>
 #include <cstdint>
 
-// Structure of a memory control block
-typedef struct mcb {
-	struct mcb   *prev; // Previous buffer head memory control block
-	struct mcb   *next; // Next buffer tail memory control block
-	std::size_t   size; // Current buffer size
-	bool          free; // True for free buffers
-} mcb_t;
+// Prologue of the buffer
+typedef struct prol {
+	struct prol *next; // Pointer to the next free buffer. The buffer is allocated if NULL
+	std::size_t  size; // Size of the buffer
+} prol_t;
 
-#define MCB_SIZE sizeof(mcb_t)
+// Epilogue of the buffer
+typedef struct epil {
+	prol_t      *prologue; // Pointer to the prologue of the buffer
+} epil_t;
 
-inline mcb_t* get_mcb(void *ptr, std::size_t offset = 0)
-{
-	return reinterpret_cast<mcb_t*>(
-		reinterpret_cast<std::uint8_t*>(ptr) + offset
-		);
+// Converts the universal pointer to a structure pointer using offset in bytes
+template<typename T> inline T *strt(void *ptr, std::int64_t offset = 0) {
+	return reinterpret_cast<T*>(reinterpret_cast<std::uint8_t*>(ptr) + offset);
 }
 
-inline mcb_t* get_tail(mcb_t *head)
-{
-	return reinterpret_cast<mcb_t*>(
-		reinterpret_cast<std::uint8_t*>(head) + head->size - MCB_SIZE
-		);
-}
+// Head of the free buffers list
+prol_t *head;
 
-mcb_t *head;
+// Tail of the free buffers list
+prol_t *tail;
 
 /* Creates the initial memory buffer */
 void mysetup(void *buf, std::size_t size)
 {
-	auto head = get_mcb(buf);
-	auto tail = get_mcb(buf, size - MCB_SIZE);
-	head->prev = tail->prev = NULL;
-	head->next = tail->next = NULL;
-	head->size = tail->size = size - MCB_SIZE * 2;
-	::head = head;
+	// Initialize the prologue of the initial buffer
+	auto prologue = strt<prol_t>(buf);
+	prologue->next = prologue;
+	prologue->size = size - sizeof(prol_t) - sizeof(epil_t);
+
+	// Initialize the epilogue of the initial buffer
+	strt<epil_t>(prologue + 1, prologue->size)->prologue = prologue;
+
+	// Initialize the free buffers list
+	head = tail = prologue;
 }
 
 /* Allocates a memory buffer of "size" bytes */
 void *myalloc(std::size_t size)
 {
-	auto curr_h = head;
-	auto curr_t = get_tail(head);
+	if (!size || !head)
+		return NULL;
 
-	while (curr_h)
-	{
-		if (curr_h->size < size)
-		{
-			curr_h = curr_h->next;
-		}
-		else if (curr_h->size == size)
-		{
+	prol_t
+		*prev = NULL,
+		*curr = head;
 
-		}
-		else
-		{
+	do {
+		if (curr->size >= size)
+			if (curr->size <= sizeof(epil_t) + sizeof(prol_t) + size) {
+				if (curr == head)
+					if (curr == tail)
+						head = tail = NULL;
+					else
+						head = curr->next;
+				else
+					prev->next = curr->next;
 
-		}
-	}
+				curr->next = NULL;
+
+				return curr + 1;
+			}
+			else {
+				// Decrease current buffer size
+				curr->size -= sizeof(epil_t) + sizeof(prol_t) + size;
+
+				// Initialize the epilogue of the current buffer
+				auto epilogue = strt<epil_t>(curr + 1, curr->size);
+				epilogue->prologue = curr;
+
+				// Initialize the prologue of the allocated buffer
+				auto prologue = strt<prol_t>(epilogue + 1);
+				prologue->next = NULL;
+				prologue->size = size;
+				
+				// Initialize the epilogue of the allocated buffer
+				strt<epil_t>(prologue + 1, size)->prologue = prologue;
+
+				return prologue + 1;
+			}
+
+		prev = curr;
+		curr = curr->next;
+	} while (prev != tail);
+
+	return NULL;
 }
 
 /* Releases the memory buffer located at "p" */
 void myfree(void *p)
 {
-	
+	auto curr = strt<prol_t>(p, -static_cast<std::int64_t>(sizeof(prol_t)));
+	auto prev = strt<epil_t>(curr, -static_cast<std::int64_t>(sizeof(epil_t)))->prologue;
+	auto next = strt<prol_t>(curr + 1, curr->size);
+
+	if (prev->next) {
+		prev->size += sizeof(epil_t) + sizeof(prol_t) + curr->size;
+		strt<epil_t>(prev + 1, prev->size)->prologue = prev;
+		curr = prev;
+	}
+
+	if (curr != tail && next->next) {
+		if (next == tail) {
+			curr->next = curr;
+			tail = curr;
+		} else
+			curr->next = next->next;
+
+		curr->size += sizeof(epil_t) + sizeof(prol_t) + next->size;
+		strt<epil_t>(curr + 1, curr->size)->prologue = curr;
+	}
 }
 
 /* 4 Mb size constant */
@@ -74,6 +121,22 @@ int main()
 {
 	// Setup the allocator
 	mysetup(malloc(SIZE), SIZE);
+
+	void *pointers[5000];
+
+	for (std::size_t j = 2; j-- > 0;) {
+		std::size_t last = 0;
+
+		for (std::size_t i = 1; i < 5000; i++)
+			if (!(pointers[i] = myalloc(i))) {
+				last = i;
+				std::cout << i << std::endl;
+				break;
+			}
+	
+		for (std::size_t i = 1; i < last; i++)
+			myfree(pointers[i]);
+	}
 
 	std::cin.get();
 	return 0;
